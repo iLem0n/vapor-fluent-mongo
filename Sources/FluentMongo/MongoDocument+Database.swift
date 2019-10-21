531ad42ce522224f60fbbfac65a4937202332845
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2019-10-21 16:37:51 +0100

    dboutput

---
+++ Sources/FluentMongo/MongoDocument+Database.swift
@@ -0,0 +1,24 @@
+//
+//  MongoDocument+Database.swift
+//  FluentMongo
+//
+//  Created by Valerio Mazzeo on 21/10/2019.
+//  Copyright © 2019 Asensei Inc. All rights reserved.
+//
+
+import Foundation
+import FluentKit
+import MongoSwift
+
+public typealias MongoDocument = Document
+
+extension Document: DatabaseOutput {
+
+    public func contains(field: String) -> Bool {
+        return self.hasKey(field)
+    }
+
+    public func decode<T>(field: String, as type: T.Type) throws -> T where T: Decodable {
+        self.
+    }
+}
